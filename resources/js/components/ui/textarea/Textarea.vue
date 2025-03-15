f3dd3f669a580df49b14fed8d8b2a79d0a13ae23
Mohammad Mustafa <mohammad.e.mustafa@gmail.com>
2025-03-15 15:56:43 +0100

    Add textarea component in frontend

---
+++ resources/js/components/ui/textarea/Textarea.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils';
+import { useVModel } from '@vueuse/core';
+import type { HTMLAttributes } from 'vue';
+
+const props = defineProps<{
+    defaultValue?: string;
+    modelValue?: string;
+    class?: HTMLAttributes['class'];
+    maxlength?: number;
+    rows?: number;
+}>();
+
+const emits = defineEmits<{
+    (e: 'update:modelValue', payload: string | number): void;
+}>();
+
+const modelValue = useVModel(props, 'modelValue', emits, {
+    passive: true,
+    defaultValue: props.defaultValue,
+});
+</script>
+
+<template>
+    <textarea
+        v-model="modelValue"
+        :class="
+            cn(
+                'flex w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+                props.class,
+            )
+        "
+        v-bind="$attrs"
+    ></textarea>
+    <p v-if="props.maxlength" class="text-sm text-gray-500 mt-1 text-right">{{ modelValue?.length ?? 0 }}/{{ props.maxlength }}</p>
+</template>
