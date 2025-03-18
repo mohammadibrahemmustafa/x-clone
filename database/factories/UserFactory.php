cd235ceea866df482bba744bc91214726c355f17
Mohammad Mustafa <mohammad.e.mustafa@gmail.com>
2025-03-18 14:49:10 +0100

    Add bio and avatar to user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'bio' => fake()->text(150),
+            'avatar' => fake()->imageUrl(),
         ];
     }
 
