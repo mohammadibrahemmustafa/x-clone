706791fa438fc3048c2bc7ad50849608c3e48129
Mohammad Mustafa <mohammad.e.mustafa@gmail.com>
2025-03-18 16:00:22 +0100

    Use constant for bio length in the user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,7 +29,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'bio' => fake()->text(150),
+            'bio' => fake()->text(config('constants.user.bio_max_length')),
             'avatar' => fake()->imageUrl(),
         ];
     }
