efbe2ce5498e075a197dcde3b882c30fac0ee3ce
Mohammad Mustafa <mohammad.e.mustafa@gmail.com>
2025-03-19 14:33:18 +0100

    Handle  uuid in the tweet model

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,9 +10,14 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Tweet extends Model
 {
     /** @use HasFactory<\Database\Factories\TweetFactory> */
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
 
     protected $fillable = [
+        'id',
         'created_by',
         'content',
     ];
